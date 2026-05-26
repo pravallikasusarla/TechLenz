0ed19446bd6ce46abbcb87e6f17644dd9d570bb1
Pravallika <pravallika@example.com>
2026-05-26 16:05:35 +0530

    Fix: Reset base configuration path for Vercel root deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TechLenz/', // <-- Add this line right here
 })

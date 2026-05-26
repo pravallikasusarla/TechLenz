09699974b145a3d99a7fdd738b9b3165d1cb0113
Pravallika <pravallika@example.com>
2026-05-26 15:59:02 +0530

    Fix: remove node_modules and update gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/TechLenz/', // <-- Add this line right here
+})
